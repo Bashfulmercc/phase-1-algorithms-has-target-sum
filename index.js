562bbd73aa9776c8c156521240ff34d2684ae689
Mason <swartmason@gmail.com>
2023-04-17 17:46:21 -0400

    lab completed

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const obj = {}
+  let diff
+
+  for (let i = 0; i < array.length; i++) {
+    diff = target - array[i]
+
+    if (obj[diff]) {
+      return true
+    } else {
+      obj[array[i]] = true
+    }
+  }
+  return false
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  O(n²)
 */
 
 /* 
-  Add your pseudocode here
+  I need to create a function that will take in an array of numbers
+  iterate through all of the numbers subtracting them against the target 
+  while checking the difference against the numbers in the array to see if they = it 
+  if they do return true if not return false
 */
 
 /*
-  Add written explanation of your solution here
+  it takes each element of the array and gets the difference between the array element and the target
+  it puts the current element into the obj as a key
+  if any element in the array matches the difference between the given number and the new element return true
+  else return false
 */
 
 // You can run `node index.js` to view these console logs
